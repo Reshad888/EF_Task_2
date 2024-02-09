238c6b57d3454dc6532f8c05693cd3621096777b
Qarax_cs20@itstep.edu.az <Qarax_cs20@itstep.edu.az>
2024-02-09 20:46:29 +0400

    Hello

---
+++ Configurations/S_CardConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Task_2.Configurations
+{
+    internal class S_CardConfiguration
+    {
+    }
+}
